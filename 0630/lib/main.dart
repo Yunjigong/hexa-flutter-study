dcaedce9a2e2bbbc2e8b9a6a9a68d1e6663899b3
Yunjigong <rhddbswl0420@naver.com>
2022-07-07 15:08:06 +0900

    Update main.dart

--- 0630/lib/main.dart
+++ 0630/lib/main.dart
@@ -1,59 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:builder/ScreenA.dart';
+import 'package:builder/ScreenB.dart';
+import 'package:builder/ScreenC.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Builder',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple),
-      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
-      );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Snack Bar'),
-        centerTitle: true,
-      ),
-      body: MySnackBar(),
-    );
-  }
-}
-
-class MySnackBar extends StatelessWidget {
-  const MySnackBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Show me'),
-        onPressed: () {
-          Scaffold.of(context)
-            .showSnackBar(SnackBar(
-              content: Text('Hellow',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white
-              ),
-              ),
-              backgroundColor: Colors.deepPurple,
-              duration: Duration(milliseconds: 1000),
-            ),
-            );
-        }),
+      title: 'navigator',
+      initialRoute: '/',
+      routes: {
+        '/':(context) => ScreenA(),
+        '/b' : (content) => ScreenB(),
+        '/c' : (content) => ScreenC(),
+      },
     );
   }
 }
